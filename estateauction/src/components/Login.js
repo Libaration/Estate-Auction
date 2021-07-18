ff083c2952cfbab7afa55666c0d1071e55381f4f
Cristian Deleon <libaration@gmail.com>
2021-07-17 23:26:55 -0400

    Set up controlled form

--- estateauction/src/components/Login.js
+++ estateauction/src/components/Login.js
@@ -12,11 +12,27 @@ export default class Login extends Component {
     return (
       <>
         <form>
-          <input type="text" name="username" />
-          <input type="password" name="password" />
+          <input
+            type="text"
+            name="username"
+            value={this.state.username}
+            onChange={(event) => this.handleChange(event)}
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={(event) => this.handleChange(event)}
+          />
           <input type="submit" value="Login" />
         </form>
       </>
     );
   }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
 }
