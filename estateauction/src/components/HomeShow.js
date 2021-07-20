e4291b8bf828830d7fd70aa6f7c729b886abe0f8
Cristian Deleon <libaration@gmail.com>
2021-07-20 04:54:40 -0400

    add more content to show component (bathroom,bedroom etc.)

--- estateauction/src/components/HomeShow.js
+++ estateauction/src/components/HomeShow.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 import { fetchHome } from '../actions/homesAction';
 import { motion } from 'framer-motion';
 import numberWithCommas from '../helpers/numbersHelper';
+import bathroom from '../icons/bathroom.png';
+import bedroom from '../icons/bedroom.png';
+import zone from '../icons/zone.png';
 
 class HomeShow extends Component {
   componentDidMount() {
@@ -13,6 +16,14 @@ class HomeShow extends Component {
     const home = this.props.homes.homes[0];
     return (
       <div className="homeShowCard">
+        <div className="homeDetails">
+          <img src={bathroom} alt="bathroom icon" />
+          <span>{home.bathrooms} Bathroom/s</span>
+          <img src={bedroom} alt="bedroom icon" />
+          <span>{home.bedrooms} Bedroom/s</span>
+          <img src={zone} alt="zone icon" />
+          <span>{home.zoning} Zoning: R8</span>
+        </div>
         <div className="bid">
           <b>Current Bid: ${numberWithCommas(home.bid)}</b>
         </div>
