3f4272da7389119272f90b7ce7f2c0b1f3448c72
Cristian Deleon <libaration@gmail.com>
2021-07-19 22:18:29 -0400

    style tweaks

--- estateauction/src/components/HomeList.js
+++ estateauction/src/components/HomeList.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Home from '../components/Home';
 import { withRouter } from 'react-router';
 import Masonry from 'react-masonry-css';
+import FadeIn from 'react-fade-in';
 function HomeList(props) {
   const renderHomes = () => {
     const { homes } = props.homes;
     return homes.map((home) => {
       return (
-        <div key={home.id}>
-          <Home home={home} key={home.id} />
-        </div>
+        <FadeIn>
+          <div key={home.id}>
+            <Home home={home} key={home.id} />
+          </div>
+        </FadeIn>
       );
     });
   };
--- estateauction/src/components/Login.js
+++ estateauction/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import logo from '../logo.png';
-
+import { motion } from 'framer-motion';
 class Login extends Component {
   constructor() {
     super();
@@ -20,37 +20,48 @@ class Login extends Component {
   render() {
     return (
       <>
-        <div className="sideText">
-          <img src={logo} alt="logo" />
-          <br></br>
-          <br></br>
-          Buy or Sell Residential and Commercial Real Estate. Leaders in Online
-          Real Estate Auction Services. Search Properties on estate auction
-        </div>
         <div className="bgHeader"></div>
-        <div className="field">
-          <form id="form_login" onSubmit={(event) => this.handleSubmit(event)}>
-            <input
-              type="text"
-              name="username"
-              autoComplete="off"
-              value={this.state.username}
-              onChange={(event) => this.handleChange(event)}
-            />
-            <br />
+        <motion.div
+          initial={{ x: -1000 }}
+          animate={{ x: 20 }}
+          className="loginContainer"
+        >
+          <div className="sideText">
+            <img src={logo} alt="logo" />
+            <br></br>
+            <br></br>
+            Buy or Sell Residential and Commercial Real Estate. Leaders in
+            Online Real Estate Auction Services. Search Properties on estate
+            auction
+          </div>
 
-            <input
-              type="password"
-              name="password"
-              value={this.state.password}
-              onChange={(event) => this.handleChange(event)}
-            />
-            <br />
-            <button type="submit" className="loginButton">
-              Login
-            </button>
-          </form>
-        </div>
+          <div className="field">
+            <form
+              id="form_login"
+              onSubmit={(event) => this.handleSubmit(event)}
+            >
+              <input
+                type="text"
+                name="username"
+                autoComplete="off"
+                value={this.state.username}
+                onChange={(event) => this.handleChange(event)}
+              />
+              <br />
+
+              <input
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={(event) => this.handleChange(event)}
+              />
+              <br />
+              <button type="submit" className="loginButton">
+                Login
+              </button>
+            </form>
+          </div>
+        </motion.div>
       </>
     );
   }
