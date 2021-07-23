249fd07a923ea1c9f5846c84707a888c54088637
Cristian Deleon <libaration@gmail.com>
2021-07-23 05:57:24 -0400

    add content to bidding component

--- estateauction/src/components/home/HomeBid.js
+++ estateauction/src/components/home/HomeBid.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import cancel from '../../icons/cancel.png';
 import { motion } from 'framer-motion';
+import bidding from '../../icons/bidding.png';
+
+import numberWithCommas from '../../helpers/numbersHelper';
 export default function HomeBid(props) {
-  const renderBids = () => {};
+  const renderBids = () => {
+    return props.home.bids.map((bid) => {
+      return (
+        <div className="bidder">
+          <ul>
+            <li>
+              ${numberWithCommas(bid.amount)} by: {bid.user}
+            </li>
+          </ul>
+        </div>
+      );
+    });
+  };
   return (
     <div className="bidOverlay">
       <motion.div
@@ -12,6 +27,14 @@ export default function HomeBid(props) {
         exit={{ y: -1500 }}
       >
         <div className="bidCard">
+          <div className="bidList">
+            <div className="mostRecent">
+              <img src={bidding} alt="bidding" />
+              Most Recent Bids:
+            </div>
+            <br />
+            {renderBids()}
+          </div>
           <div className="cancelIcon">
             <img src={cancel} alt="cancel" onClick={props.toggleVisible} />
           </div>
