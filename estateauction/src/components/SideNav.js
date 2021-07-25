b2448cbace97d0ec227142c2e993ff4806f4a773
Cristian Deleon <libaration@gmail.com>
2021-07-25 07:44:17 -0400

    add profile link to route in sidenav component

--- estateauction/src/components/SideNav.js
+++ estateauction/src/components/SideNav.js
@@ -28,6 +28,7 @@ export default function SideNav(props) {
           <Link to="/">Home</Link>
           <Link to="/homes">Auctions</Link>
           <Link to="/homes/add">List New Home</Link>
+          <Link to="/profile">My Profile</Link>
           {props.loggedIn ? (
             <Link to="/logout">Logout</Link>
           ) : (
