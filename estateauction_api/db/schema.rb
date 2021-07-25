53ab0c5a74f10449cd0e291c73a3ea87f3a5002d
Cristian Deleon <libaration@gmail.com>
2021-07-25 03:34:52 -0400

    add url column to users model in backend / add userdetails component

--- estateauction_api/db/schema.rb
+++ estateauction_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_24_085506) do
+ActiveRecord::Schema.define(version: 2021_07_25_073001) do
 
   create_table "bids", force: :cascade do |t|
     t.integer "amount"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_07_24_085506) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "url"
   end
 
   add_foreign_key "homes", "users"
