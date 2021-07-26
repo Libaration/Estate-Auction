43104eeebe8b78f93e70eab07deb21cdbe7cdf82
Cristian Deleon <libaration@gmail.com>
2021-07-26 03:33:17 -0400

    finish usercontainer rough implementation

--- estateauction/src/containers/UserContainer.tsx
+++ estateauction/src/containers/UserContainer.tsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import UserDetails from '../components/user/UserDetails';
 import { fetchHomes } from '../actions/homesAction';
+import Home from '../components/home/Home';
+import logo from '../logo.png';
 
 interface Props {
   user: {
@@ -11,25 +13,49 @@ interface Props {
     url: string;
     bids?: [];
   };
-  homes: {
-    id: number;
-    address: string;
-  };
+  homes: { homes: Homes[] };
   fetchHomes: (id: string) => void;
+}
+interface Homes {
+  id: number;
+  address: string;
 }
 
 class UserContainer extends Component<Props> {
   componentDidMount() {
     const { user } = this.props;
-    this.props.fetchHomes(user.id as unknown as string);
+    this.props.fetchHomes((user.id as unknown as string) || 'all');
   }
+
+  renderHomes = () => {
+    return (
+      <>
+        {this.props.homes.homes.map((home) => {
+          return (
+            <Home
+              key={home.id}
+              home={home}
+              viewOptions={{ willShowBids: false, willShowCountdown: false }}
+            />
+          );
+        })}
+      </>
+    );
+  };
   render() {
+    const { homes } = this.props.homes;
+    const haveHomesLoaded: boolean = !!homes.length ? true : false;
     return (
       <>
         <div className="userShowField">
           <div className="homeShowCard">
+            <div className="userShowLogo">
+              <img src={logo} alt="logo" />
+            </div>
             <UserDetails user={this.props.user} />
-            <div className="myHomes">esdkdnkdnkdnekdnk</div>
+            <div className="myHomes">
+              {haveHomesLoaded ? this.renderHomes() : '....Loading'}
+            </div>
           </div>
         </div>
       </>
@@ -40,6 +66,7 @@ class UserContainer extends Component<Props> {
 const mapStateToProps = (state: Props) => {
   return {
     user: state.user,
+    homes: state.homes,
   };
 };
 
